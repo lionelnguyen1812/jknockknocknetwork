a25a921b37237d49a0e9cdd8e322ccf9b0f2d576
Van <Van@VanAnh>
2013-08-17 09:04:46 +0700

    va comit new file

---
+++ KnokKnokClient/src/knokknokclient/VaTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package knokknokclient;
+
+/**
+ *
+ * @author Van
+ */
+public class VaTest {
+    //cach 2 la add băng netbean luon, that ra la netbean nó tự chạy dòng lệnh cho e thôi
+    //add xong rồi thì phải comit lan dau tien
+    
+}
